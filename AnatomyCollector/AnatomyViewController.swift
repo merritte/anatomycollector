f1a0fbe1516382a97ffb73b0fcc323297fb5bac1
Shay-Jahen Merritte <Shay@Merritte.com>
2016-10-18 18:36:46 -0400

    linked up AnatomyViewController

---
+++ AnatomyCollector/AnatomyViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AnatomyViewController.swift
+//  AnatomyCollector
+//
+//  Created by Merritte on 10/18/16.
+//  Copyright © 2016 Merritte. All rights reserved.
+//
+
+import UIKit
+
+class AnatomyViewController: UIViewController {
+
+    @IBOutlet weak var anatomyImageView: UIImageView!
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func photosTapped(_ sender: AnyObject) {
+    }
+    
+    @IBAction func cameraTapped(_ sender: AnyObject) {
+    }
+    
+    @IBAction func addTapped(_ sender: AnyObject) {
+    }
+}
